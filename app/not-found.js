98b99558e6c41abee690591e2f029368d1e4e2db
Rajneesh Kumar <rishulpu1@gmail.com>
2025-09-08 01:48:56 +0530

    error handling and page not found

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>404 Not Found</h1>
+      <p>Page you are looking for not found.</p>
+    </main>
+  );
+}
